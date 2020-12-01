31855d8a2b5ea75c97ed7be3cbd3b3d824e32692
jonny sywulak <jonathan.sywulak@gmail.com>
2020-12-01 17:51:35 +0000

    tabs to spaces but now that i think about it that is not really what i meant to do

--- 01/01.rb
+++ 01/01.rb
@@ -3,23 +3,23 @@ file_data = file.readlines.map(&:chomp)
 file.close
 
 file_data.each do |fnumber|
-	difference = 2020 - fnumber.to_i
-	
-	if file_data.include?(difference.to_s)
-		puts "d[#{difference}], f_num[#{fnumber}], p[#{(difference * fnumber.to_i)}]" 
-		break
-	end
+  difference = 2020 - fnumber.to_i
+    
+  if file_data.include?(difference.to_s)
+    puts "d[#{difference}], f_num[#{fnumber}], p[#{(difference * fnumber.to_i)}]" 
+    break
+  end
 end
 
 found = false
 file_data.each do |fnumber|
-	file_data.each do |snumber|
-		difference = 2020 - fnumber.to_i - snumber.to_i		
-		if file_data.include?(difference.to_s)
-			found = true
-			puts "d[#{difference}], f_num[#{fnumber}], s_num[#{snumber}], p[#{(difference * fnumber.to_i * snumber.to_i)}]" 
-			break
-		end
-		break if found
-	end
+  file_data.each do |snumber|
+    difference = 2020 - fnumber.to_i - snumber.to_i     
+    if file_data.include?(difference.to_s)
+      found = true
+      puts "d[#{difference}], f_num[#{fnumber}], s_num[#{snumber}], p[#{(difference * fnumber.to_i * snumber.to_i)}]" 
+      break
+    end
+    break if found
+  end
 end
