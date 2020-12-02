15ad7ebe88787385bfcf92eb3d1950b4d34e0eea
jonny sywulak <jonathan.sywulak@gmail.com>
2020-12-02 18:11:16 +0000

    part two

--- 02/02.rb
+++ 02/02.rb
@@ -12,3 +12,21 @@ file_data.each do |line|
 	num_valid += 1 if min <= count && count <= max
 end
 puts num_valid
+
+num_valid = 0
+file_data.each do |line|
+	tuple = line.split(' ')
+	pos1 = tuple[0].split('-')[0].to_i - 1
+	pos2 = tuple[0].split('-')[1].to_i - 1
+	letter = tuple[1].split(':')[0]
+	password = tuple[2]
+
+
+	if (password[pos1] == letter || password[pos2] == letter) 
+		if (password[pos1] != password[pos2])
+			num_valid += 1
+		end
+	end	
+	
+end
+puts num_valid
