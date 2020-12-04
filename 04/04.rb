b1b711ebabd86fd0032b99104d5d8c90aa8b92a8
jonny sywulak <jonathan.sywulak@gmail.com>
2020-12-04 17:36:35 +0000

    this one is real bad

--- 04/04.rb
+++ 04/04.rb
@@ -1,38 +1,64 @@
 def is_valid_passport? passport
 	result = true
 
-	is_valid_birth_year? passport
-	is_valid_issue_year? passport
-	is_valid_expir_year? passport
-	is_valid_height? passport
-	is_valid_hair_color? passport
-	is_valid_eye_color? passport
-	is_valid_passport_id? passport
+	result = result && (is_valid_birth_year? passport)
+	result = result && (is_valid_issue_year? passport)
+	result = result && (is_valid_expir_year? passport)
+	result = result && (is_valid_height? passport)
+	result = result && (is_valid_hair_color? passport)
+	result = result && (is_valid_eye_color? passport)
+	result = result && (is_valid_passport_id? passport)
 
 	return result
 end
 
 
 def is_valid_birth_year? passport
-	true
+	result = true
+	result = false if passport['byr'].to_i < 1920
+	result = false if passport['byr'].to_i > 2002
+	return result
 end
 def is_valid_issue_year? passport
-	true
+	result = true
+	result = false if passport['iyr'].to_i < 2010
+	result = false if passport['iyr'].to_i > 2020
+	return result
 end
 def is_valid_expir_year? passport
-	true
+	result = true
+	result = false if passport['eyr'].to_i < 2020
+	result = false if passport['eyr'].to_i > 2030
+	return result
 end
 def is_valid_height? passport
-	true
+	result = true
+	if passport['hgt'].include? "cm"
+		cms = passport['hgt'][0...-2].to_i
+		result = false if cms < 150
+		result = false if cms > 193
+	elsif passport['hgt'].include? "in"
+		ins = passport['hgt'][0...-2].to_i
+		result = false if ins < 59
+		result = false if ins > 76		
+	else 
+		result = false
+	end
+	return result
 end
 def is_valid_hair_color? passport
-	true
+	result = true
+	result = false if passport['hcl'][0] != '#'
+	result = false if passport['hcl'].length != 7
+	result = false if passport['hcl'][1...7].downcase =~ ["g-z"]
+	return result
 end
+# 
 def is_valid_eye_color? passport
-	true
+	%w(amb blu brn gry grn hzl oth).include?(passport['ecl'])
 end
 def is_valid_passport_id? passport
-	true
+	return passport['pid'].length == 9
 end
 
 
