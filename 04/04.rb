58d7c037b009aeeeec1fe79267d10b7fc1a72e5c
jonny sywulak <jonathan.sywulak@gmail.com>
2020-12-04 17:49:54 +0000

    less terrible

--- 04/04.rb
+++ 04/04.rb
@@ -53,60 +53,79 @@ def is_valid_hair_color? passport
 	result = false if passport['hcl'][1...7].downcase =~ ["g-z"]
 	return result
 end
-# 
+
+
 def is_valid_eye_color? passport
 	%w(amb blu brn gry grn hzl oth).include?(passport['ecl'])
 end
+
 def is_valid_passport_id? passport
 	return passport['pid'].length == 9
 end
 
 
-file = File.open("input.txt")
-file_data = file.readlines.map(&:chomp)
-file.close
-
-
-raw_passports = []
-current = ""
-
-file_data.each do |line|
-	current << " #{line} "
-	if line.to_s.strip.empty?
-		raw_passports << " #{current} "
-		current = ""
+def parse_raw_passport_data data
+	raw_passports = []
+	current = ""
+	data.each do |line|
+		current << " #{line} "
+		if line.to_s.strip.empty?
+			raw_passports << " #{current} "
+			current = ""
+		end
 	end
+	return raw_passports
 end
 
-passports = []
-raw_passports.each do |raw_passport|
-	result = {}
-	kvs = raw_passport.gsub(/\s+/m, ' ').strip.split(" ")
-	kvs.each do |kv|
-		result[kv.split(':')[0]] = kv.split(':')[1]
+def parse_passport_data raw_passports
+	passports = []
+	raw_passports.each do |raw_passport|
+		result = {}
+		kvs = raw_passport.gsub(/\s+/m, ' ').strip.split(" ")
+		kvs.each do |kv|
+			result[kv.split(':')[0]] = kv.split(':')[1]
+		end
+		passports << result
 	end
-	passports << result
+	return passports
 end
 
-valid_count = 0
-almost_valid_count = 0
-
-review = []
-
-passports.each do |passport| 
-	if (passport.length == 8) || (passport.length == 7 && passport['cid'].nil?)
-		valid_count += 1
-		review << passport
+def parse_correct_number_of_fields passports
+	result = []
+	passports.each do |passport| 
+		if (passport.length == 8) || (passport.length == 7 && passport['cid'].nil?)
+			result << passport
+		end
 	end
+	return result
 end
+
+def parse_valid_data_in_fields passports
+	result = []
+	passports.each do |passport| 
+		result << passport if is_valid_passport? passport
+	end
+	return result
+end
+
+def get_input_data
+	file = File.open("input.txt")
+	file_data = file.readlines.map(&:chomp)
+	file.close
+
+	return file_data
+end
+
+
+file_data = get_input_data
+
+raw_passports = parse_raw_passport_data file_data
+passports = parse_passport_data raw_passports
+valid_passports = parse_correct_number_of_fields passports
+really_valid_passports = parse_valid_data_in_fields valid_passports
 
 puts "total passports: #{passports.length}"
-puts "number valid: #{valid_count}"
-
-valid_count = 0
-review.each do |passport| 
-	valid_count += 1 if is_valid_passport? passport
-end
-puts "number valid: #{valid_count}"
+puts "number valid: #{valid_passports.length}"
+puts "number valid: #{really_valid_passports.length}"
 
 
