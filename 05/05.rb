cdd62a6012b30b28d75a8b0ca47f7e41ec4176a5
jonny sywulak <jonathan.sywulak@gmail.com>
2020-12-06 00:06:01 +0000

    i am about to get crazy

--- 05/05.rb
+++ 05/05.rb
@@ -28,7 +28,9 @@ def parse_seat_data input
 	result = {}
 	result[:row] = row input
 	result[:col] = col input
+	result[:raw] = (convert_to_binary_rep input).to_i(base=2)
 	result[:sid] = result[:row] * 8 + result[:col]
+	puts result
 	result
 end
 
@@ -44,32 +46,87 @@ end
 puts max
 
 seats = []
+ids = []
 file_data.each do |line|
 	seat = parse_seat_data line
 	seats << seat
+	ids << (seat[:sid] - 40)
 end
 
-seats_per_row = {}
-
-seats.each do |seat|
-  if seats_per_row[seat[:row]].nil?
-  	seats_per_row[seat[:row]] = 0
-  end
-  seats_per_row[seat[:row]] += 1
-end
-
-seats_per_row.each do |row, seats|
-	if seats != 8
-		puts "#{row} is missing a seat"
+sorted_ids = ids.sort
+puts sorted_ids
+0...sorted_ids.length do |i|
+	if (sorted_ids.include? (i - 1)) && (sorted_ids.cd? (i + 1)) && (!sorted_ids.include? i)
+		puts "okay it's this one: #{i}"
 	end
 end
 
-seats.each do |seat|
-	if seat[:row] == 75
-		puts seat[:col]
-	end
-end
 
-puts seats_per_row
+# seat_map = Array.new(128, Array.new(8, false))
+# seats.each do |seat|
+# 	row = seat[:row]
+# 	col = seat[:col]
+# 	seat_map[row][col] = true
+# 	puts "[#{seat_map}]"
+# end
 
-puts 75 * 8 + 7
+# puts "hello #{seat_map[75]}"
+# seat_map.each do |row|
+# 	if !row.empty?
+# 		puts "#{row} is not full"
+# 	end
+# end
+# puts "#{seat_map.length}"
+
+# seat_map = Array.new(128, Array.new(8, false))
+# seats.each do |seat|
+# 	row = seat[:row]
+# 	col = seat[:col]
+# 	seat_map[row][col] = true
+# end
+
+# seat_map.each do |row|
+# 	empty_seats = row.detect { |col| col == false}
+# 	if !empty_seats.nil?
+# 		puts "seat r:#{row} c:#{col} is empty"
+# 	end
+# end
+
+
+
+# seat_map = {}
+# seats.each do |seat|
+# 	if seat_map[seat[:row]].nil?
+# 		seat_map[seat[:row]] = [8]
+# 	end
+# 	seat_map[seat[:row]][seat[:col]] = "X"
+# end
+
+# seat_map.each do |row, seats|
+# 	puts "#{row}, #{seats.flatten}"
+# end
+
+# seats_per_row = {}
+
+# seats.each do |seat|
+#   if seats_per_row[seat[:row]].nil?
+#   	seats_per_row[seat[:row]] = 0
+#   end
+#   seats_per_row[seat[:row]] += 1
+# end
+
+# seats_per_row.each do |row, seats|
+# 	if seats != 8
+# 		puts "#{row} is missing a seat"
+# 	end
+# end
+
+# seats.each do |seat|
+# 	if seat[:row] == 75
+# 		puts seat[:col]
+# 	end
+# end
+
+# puts seats_per_row
+
+# puts 75 * 8 + 7
